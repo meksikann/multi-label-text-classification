9b8add3659b4afae8fc34a08a29ffa50fe6c5b4a
serhii <meksikann@gmail.com>
2020-02-20 11:28:04 +0200

    build classifier

--- classifier/train.py
+++ classifier/train.py
@@ -18,6 +18,8 @@ MODEL_PATH = 'export.pkl'
 dir_path = path.dirname(path.realpath(__file__))
 X_COL = 'text'
 Y_COL = 'labels'
+BATCH_SIZE = 48
+
 
 '''
 Training JSON data should be next format
@@ -57,7 +59,6 @@ def prepare_vectors():
     learn_head_path = path.join(dir_path, 'models', 'fit_head')
     learn_lm_path = path.join(dir_path, 'models', LEARNER_NAME)
     learn_encoder_path = path.join(dir_path, 'models', LEARNER_ENCODER_NAME)
-    model_path = path.join(dir_path, 'models', MODEL_PATH)
 
     # paste dataset into data directory and name it training.csv
 
@@ -94,14 +95,12 @@ def prepare_vectors():
     # concat all data for LM
     df_lm = pd.concat([df_train, df_valid], ignore_index=True)
 
-    batch_size = 48
-
     # CASE1 - create LM with TextList
 
-    data_lm = (TextList.from_df(df, path=dir_path, cols=X_COL)
+    data_lm = (TextList.from_df(df_lm, path=dir_path, cols=X_COL)
                .split_by_rand_pct(0.1)
                .label_for_lm()
-               .databunch(bs=batch_size))
+               .databunch(bs=BATCH_SIZE))
 
     # save LM
     data_lm.save(temp_lm_path)
@@ -127,7 +126,77 @@ def prepare_vectors():
     learn_lm.save(learn_lm_path)
     learn_lm.save_encoder(learn_encoder_path)
 
-    
+    '''
+    ********************  Classifier ***********************************
+    '''
+
+    class_data_lm = TextLMDataBunch(dir_path, temp_lm_path, bs=BATCH_SIZE)
+
+    # functions for metricts
+    def precision(log_preds, targs, thresh=0.5, epsilon=1e-8):
+        pred_pos = (log_preds > thresh).float()
+        tpos = torch.mul((targs == pred_pos).float(), targs.float())
+        return (tpos.sum() / (pred_pos.sum() + epsilon))  # .item()
+
+    def recall(log_preds, targs, thresh=0.5, epsilon=1e-8):
+        pred_pos = (log_preds > thresh).float()
+        tpos = torch.mul((targs == pred_pos).float(), targs.float())
+        return (tpos.sum() / (targs.sum() + epsilon))
+
+    data_class = TextClasDataBunch.from_df(
+        dir_path,
+        train_df=df_train,
+        valid_df=df_valid,
+        vocab=class_data_lm.vocab,
+        text_cols='text',
+        label_cols='labels',
+        label_delim='|',
+        bs=BATCH_SIZE
+    )
+
+    data_class.save('tmp_class')
+
+    learn = text_classifier_learner(data_class, drop_mult=0.5)
+
+    # define metrics
+    learn.metrics = [accuracy_thresh, precision, recall]
+
+    learn.load_encoder(learn_encoder_path)
+
+    learn.freeze()
+    learn.fit_one_cycle(1, 3e-2, moms=(0.8, 0.7))
+
+    # save first part trained learner
+    learn.save('first_training')
+    learn.load('first_training')
+
+    learn.freeze_to(-2)
+    learn.fit_one_cycle(2, slice(1e-2/(2.6**4), 1e-2), moms=(0.8, 0.7), wd=0.1)
+
+    learn.save('second_training')
+    learn.load('second_training')
+
+    learn.unfreeze()
+    learn.fit_one_cycle(2, slice(1e-3/(2.6**4),1e-3), moms=(0.8,0.7), wd=0.)
+
+    model.export(path.join(dir_path, 'models', MODEL_PATH))
+
+    # get validation set data
+
+    # Evaluate results
+    y_pred, y_true = learn.get_preds()
+    from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score
+    from sklearn.metrics import classification_report
+    f1_score(y_true, y_pred > 0.35, average='micro')
+    y_true = y_true.numpy()
+    scores = y_pred.numpy()
+
+    print(scores.shape, y_true.shape)
+    metrics = classification_report(y_true, scores > 0.35, target_names=data_clas.valid_ds.classes)
+    print(metrics)
+
+
+
 
     # CASE2 - create LM with TextLMDataBunch
     # # create language model data bunch with vector representation of all unique words as tokens
