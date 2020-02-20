74766712a35ac8d9eece85056347174283cafdb3
serhii <meksikann@gmail.com>
2020-02-20 14:24:08 +0200

    classifier done

--- classifier/train.py
+++ classifier/train.py
@@ -18,8 +18,11 @@ MODEL_PATH = 'export.pkl'
 dir_path = path.dirname(path.realpath(__file__))
 X_COL = 'text'
 Y_COL = 'labels'
-BATCH_SIZE = 48
 
+# tuning params
+BATCH_SIZE = 32
+VAL_PERC = 0.8
+MICRO_DS = 200
 
 '''
 Training JSON data should be next format
@@ -54,13 +57,9 @@ def prepare_vectors():
     print('Start training')
     print('TF_KERAS:', os.environ.get('TF_KERAS'))
 
-    training_set_path = path.join(dir_path, 'data', training_set_name)
-    temp_lm_path = path.join(dir_path, 'data', TEMP_LM_NAME)
-    learn_head_path = path.join(dir_path, 'models', 'fit_head')
-    learn_lm_path = path.join(dir_path, 'models', LEARNER_NAME)
-    learn_encoder_path = path.join(dir_path, 'models', LEARNER_ENCODER_NAME)
+    my_path = Path('data')
 
-    # paste dataset into data directory and name it training.csv
+    training_set_path = path.join(dir_path, 'data', training_set_name)
 
     with open(training_set_path) as file:
         data = json.load(file)
@@ -78,10 +77,10 @@ def prepare_vectors():
     df = shuffle(df)
 
     # create small(micro) dataset to simulate real world situation, where no big dataset available in the company
-
-    micro_df = df[:200]
+    print('Make micro dataset')
+    micro_df = df[:MICRO_DS]
     # split on train test datasets
-    split_v = int(0.6 * len(micro_df)) + 1
+    split_v = int(VAL_PERC * len(micro_df)) + 1
 
     # split dataset on 80/20
     df_train, df_valid = micro_df[:split_v], micro_df[split_v:]
@@ -97,40 +96,47 @@ def prepare_vectors():
 
     # CASE1 - create LM with TextList
 
-    data_lm = (TextList.from_df(df_lm, path=dir_path, cols=X_COL)
+    print('Create Language Model from dataset')
+    data_lm = (TextList.from_df(df_lm, path=my_path, cols=X_COL)
                .split_by_rand_pct(0.1)
                .label_for_lm()
                .databunch(bs=BATCH_SIZE))
 
     # save LM
-    data_lm.save(temp_lm_path)
+    data_lm.save('tmp_lm')
+
+    print('Learn Language Model')
 
     learn_lm = language_model_learner(data_lm, AWD_LSTM, drop_mult=0.2)
     learn_lm.lr_find()
-    learn_lm.recorder.plot()
+    # learn_lm.recorder.plot()
 
     learn_lm.fit_one_cycle(1, 1e-2, moms=(0.8, 0.7))
 
     # save trained learner
-    learn_lm.save(learn_head_path)
-    learn_lm.load(learn_head_path)
+    learn_lm.save('fit_head')
+    learn_lm.load('fit_head')
 
     # learn other layers
     learn_lm.unfreeze()
-    learn_lm.fit_one_cycle(10, 1e-3, moms=(0.8, 0.7))
+    learn_lm.fit_one_cycle(20, 2e-5, moms=(0.8, 0.7))
 
     # plot learning results
-    learn_lm.recorder.plot_losses()
+    # learn_lm.recorder.plot_losses()
+    print('Save Language Model')
 
     # save trained learner and encoder
-    learn_lm.save(learn_lm_path)
-    learn_lm.save_encoder(learn_encoder_path)
+    learn_lm.save('fine_tuned')
+    learn_lm.save_encoder('fine_tuned_enc')
 
     '''
     ********************  Classifier ***********************************
     '''
+    print('Build classifier')
+    print('Load data')
 
-    class_data_lm = TextLMDataBunch(dir_path, temp_lm_path, bs=BATCH_SIZE)
+    # class_data_lm = TextLMDataBunch.load(my_path, 'tmp_lm', bs=BATCH_SIZE)
+    class_data_lm = load_data(my_path, 'tmp_lm', bs=BATCH_SIZE)
 
     # functions for metricts
     def precision(log_preds, targs, thresh=0.5, epsilon=1e-8):
@@ -144,7 +150,7 @@ def prepare_vectors():
         return (tpos.sum() / (targs.sum() + epsilon))
 
     data_class = TextClasDataBunch.from_df(
-        dir_path,
+        my_path,
         train_df=df_train,
         valid_df=df_valid,
         vocab=class_data_lm.vocab,
@@ -155,35 +161,38 @@ def prepare_vectors():
     )
 
     data_class.save('tmp_class')
+    print('Init classifier learner')
 
-    learn = text_classifier_learner(data_class, drop_mult=0.5)
+    learn = text_classifier_learner(data_class, AWD_LSTM, drop_mult=0.3)
 
     # define metrics
-    learn.metrics = [accuracy_thresh, precision, recall]
+    # learn.metrics = [accuracy_thresh, precision, recall]
 
-    learn.load_encoder(learn_encoder_path)
+    learn.load_encoder('fine_tuned_enc')
+    print('Train classifier learner')
 
     learn.freeze()
-    learn.fit_one_cycle(1, 3e-2, moms=(0.8, 0.7))
+    learn.fit_one_cycle(1, 2e-5, moms=(0.8, 0.7))
 
     # save first part trained learner
-    learn.save('first_training')
-    learn.load('first_training')
+    learn.save('first_factors')
+    learn.load('first_factors')
 
     learn.freeze_to(-2)
-    learn.fit_one_cycle(2, slice(1e-2/(2.6**4), 1e-2), moms=(0.8, 0.7), wd=0.1)
+    learn.fit_one_cycle(2, slice(1e-2 / (2.6 ** 4), 1e-2), moms=(0.8, 0.7), wd=0.1)
 
-    learn.save('second_training')
-    learn.load('second_training')
+    learn.save('second_factors')
+    learn.load('second_factors')
 
     learn.unfreeze()
-    learn.fit_one_cycle(2, slice(1e-3/(2.6**4),1e-3), moms=(0.8,0.7), wd=0.)
+    learn.fit_one_cycle(2, slice(1e-3 / (2.6 ** 4), 1e-3), moms=(0.8, 0.7), wd=0.1)
+    print('Classifier done. Export final model.')
 
-    model.export(path.join(dir_path, 'models', MODEL_PATH))
-
-    # get validation set data
+    learn.export(path.join(dir_path, 'models', MODEL_PATH))
 
     # Evaluate results
+    print('Build training metrics')
+
     y_pred, y_true = learn.get_preds()
     from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score
     from sklearn.metrics import classification_report
@@ -192,52 +201,16 @@ def prepare_vectors():
     scores = y_pred.numpy()
 
     print(scores.shape, y_true.shape)
-    metrics = classification_report(y_true, scores > 0.35, target_names=data_clas.valid_ds.classes)
+    metrics = classification_report(y_true, scores > 0.35, target_names=data_class.valid_ds.classes)
     print(metrics)
 
 
-
-
-    # CASE2 - create LM with TextLMDataBunch
-    # # create language model data bunch with vector representation of all unique words as tokens
-    # data_lm = TextLMDataBunch.from_df(
-    #     path=dir_path,
-    #     train_df=df_train,
-    #     valid_df=df_valid,
-    #     label_cols='Sentiment_Score',
-    #     text_cols='Tweet'
-    # )
-    #
-    # data_lm.save(path.join(dir_path, 'data',  PICKLE_VECTORS))
-    #
-    # language_model = language_model_learner(data_lm, AWD_LSTM, drop_mult=0.3)
-    #
-    # language_model.save_encoder(path.join(dir_path, 'data',  LEARNENR_PATH))
-    #
-    # data_class = TextClasDataBunch.from_df(
-    #     path=dir_path,
-    #     train_df=df_train,
-    #     valid_df=df_valid,
-    #     vocab=data_lm.train_ds.vocab,
-    #     label_cols='Sentiment_Score',
-    #     text_cols='Tweet'
-    # )
-    #
-    # model = text_classifier_learner(data_class, AWD_LSTM, drop_mult=0.3)
-    # model.load_encoder(path.join(dir_path, 'data',  LEARNENR_PATH))
-    #
-    # model.fit_one_cycle(1, max_lr=1e-1)
-    #
-    # # find good learning rate
-    # model.lr_find()
-    #
-    # # model.recorder.plot(suggestion=True)
-    #
-    # model.export(path.join(dir_path, 'models', MODEL_PATH))
-
-
 def predict(text):
+    print('Load classifier')
+
     model = load_learner(path.join(dir_path, 'models'))
+    print('Start prediction')
+
     pred = model.predict(text)
 
     print(pred)
